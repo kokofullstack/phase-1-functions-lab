1faaf4a3af45df003e4ed42a80a9b92572f81da1
Maung Ko Ko <mwkoko2020@gmail.com>
2023-03-20 21:19:47 -0600

    Complete the lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const distance = 50;
+function distanceFromHqInBlocks(distance){
+    return distance > 42 ? distance - 42 : 42 - distance;
+};
+
+function distanceFromHqInFeet(distance){
+    distanceFromHqInBlocks(distance);
+    return distance > 42 ? (distance - 42) * 264:
+           distance < 42 ? (42 - distance) * 264:
+           0;
+};
+
+function distanceTravelledInFeet(start,destination){
+    return destination > start ? (destination - start) * 264:
+         (start - destination) * 264;
+};
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    return distance <= 400 ? 0:
+           distance > 400 && distance < 2000 ? (distance - 400) * 0.02:
+           distance >= 2000 && distance <= 2500 ? 25:
+           "cannot travel that far";    
+};
